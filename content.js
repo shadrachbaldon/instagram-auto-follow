0589ba257b96cee2cfc95b31cd8b74124e93c9ad
shadrachbaldon <shadrachbaldon.dev@gmail.com>
2017-05-10 18:38:49 +0800

    click follow buttons
    Clicks follow buttons every 1 second. Scroll down if there's no follow button found.

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 console.log("content.js loaded!");
-var scroolToOffset = 300;
-var x = Math.floor(Math.random() * (48 - 36 + 1)) + 36;//random number
+var scroolToOffset = 50,followInterval,followCount = 0;
 
 function scroll(){
 	$("._4gt3b").animate({
@@ -9,24 +8,44 @@ function scroll(){
 }
 
 function follow(){
-	var randomSecs = Math.floor(Math.random() * (48 - 36 + 1)) + 36;//random number
-	var randomSecs = randomSecs * 1000;
-	setTimeout(function(){
-		$("div._72gdz > span > button._84y62").click()
-	},randomSecs);
+
+	followInterval = setInterval(function(){
+		var randomSecs = Math.floor(Math.random() * (48 - 36 + 1)) + 36;//random number
+		var randomSecs = randomSecs * 1000;
+		followCount +=1;
+		if ($("div._72gdz > span > button._84y62").length === 0) {
+			console.log("button:"+$("div._72gdz > span > button._84y62").length);
+			scroolToOffset += 100
+			scroll();
+		}else{
+			$("div._72gdz > span > button._84y62").first().click();
+			console.log("button:"+$("div._72gdz > span > button._84y62").length);
+			console.log("total followed:"+followCount);
+		}
+		
+		// setTimeout(function(){
+			
+		// },10000);
+		
+	},1000);//end interval
 }
 
 $(document).ready(function(){
-	
-	
+
 });
 
 chrome.runtime.onMessage.addListener(
-	  function(request, sender, sendResponse) {
+	function(request, sender, sendResponse) {
 		console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-	    if (request.action == "start"){
-	    	console.log("start action received!");
-	    	scroll();
-			scroolToOffset += 300;
-	    }
+3
+		switch(request.action){
+			case "start":
+				console.log("start action received!");
+		    	follow();
+				scroolToOffset += 300;
+			break;
+			case "stop":
+				clearInterval(followInterval);
+			break;
+		}
 });
