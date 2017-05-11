3ad8a83b54a2b274a57ac5429871712c4574e4e0
shadrachbaldon <shadrachbaldon.dev@gmail.com>
2017-05-11 15:08:43 +0800

    Just some UI restrictions

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 console.log("content.js loaded!");
-var scroolToOffset = 50,followInterval,followCount = 0;
+var scroolToOffset = 1000,followInterval,followCount = 0;
 
 function scroll(){
 	$("._4gt3b").animate({
@@ -12,22 +12,22 @@ function follow(){
 	followInterval = setInterval(function(){
 		var randomSecs = Math.floor(Math.random() * (48 - 36 + 1)) + 36;//random number
 		var randomSecs = randomSecs * 1000;
-		followCount +=1;
+		
 		if ($("div._72gdz > span > button._84y62").length === 0) {
+			//scroll if there's no follow button found
 			console.log("button:"+$("div._72gdz > span > button._84y62").length);
-			scroolToOffset += 100
-			scroll();
+			scroolToOffset += 1000
+			scroll();scroll();
 		}else{
 			$("div._72gdz > span > button._84y62").first().click();
+			followCount +=1;
+			chrome.runtime.sendMessage({action: "updateTotalFollow"});
 			console.log("button:"+$("div._72gdz > span > button._84y62").length);
 			console.log("total followed:"+followCount);
+
 		}
 		
-		// setTimeout(function(){
-			
-		// },10000);
-		
-	},1000);//end interval
+	},2000);//end interval
 }
 
 $(document).ready(function(){
@@ -37,7 +37,7 @@ $(document).ready(function(){
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-3
+
 		switch(request.action){
 			case "start":
 				console.log("start action received!");
